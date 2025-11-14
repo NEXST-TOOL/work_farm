74e2e04107199bf86b92f77145a7b4b468602a25
Liu Shiqi <liushiqi17@mails.ucas.ac.cn>
2025-11-14 20:15:36 +0800

    fixed EXPORT_VIVADO_PATH

--- build_scripts/toolset.mk
+++ build_scripts/toolset.mk
@@ -5,7 +5,7 @@ VIVADO_TOOL_BASE ?= /opt/Xilinx_$(VIVADO_VERSION)
 # Vivado and SDK tool executable binary location
 VIVADO_TOOL_PATH := $(VIVADO_TOOL_BASE)/Vivado/$(VIVADO_VERSION)/bin
 
-EXPORT_VIVADO_PATH := export PATH=$(VIVADO_TOOL_PATH)::$$PATH
+EXPORT_VIVADO_PATH := export PATH=$(VIVADO_TOOL_PATH):$$PATH
 
 LINUX_GCC_PREFIX ?= riscv64-linux-gnu-
 
